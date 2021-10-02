36c2552ffbd651a7bb0af6f424fd8fd50a8a9a99
Eduwigsh <esolisa1900@alumno.ipn.mx>
2021-10-01 23:50:16 -0500

    trabajo

---
+++ js/cripto.js
@@ -0,0 +1,10 @@
+function elegir(){
+    if (document.getElementById("cesar").checked){
+        window.location = '01 cesar/index.html';
+        window.location.assign('01 cesar/index.html');
+    }
+    else if (document.getElementById("viggenere").checked){
+        window.location = '02 viggenere/index.html';
+        window.location.assign('02 viggenere/index.html');
+    }
+}
